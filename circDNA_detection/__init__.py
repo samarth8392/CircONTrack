5a4387fb08ad5fadb040a68d4b16a68dcddde069
Samarth Mathur <samarth.mathur@nih.gov>
2025-08-14 12:57:32 -0400

    chore: bump version to 0.3.0

--- circDNA_detection/__init__.py
+++ circDNA_detection/__init__.py
@@ -3,7 +3,7 @@
 CircONTrack: ONT-optimized Circular DNA Detection Pipeline
 """
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 __author__ = "Samarth Mathur, PhD"
 __email__ = "samarth8392@gmail.com"
 
